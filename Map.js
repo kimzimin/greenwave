8d386532972159a7ebaf5705c10a3397103f3245
che122 <106135369+che122@users.noreply.github.com>
2022-06-02 12:48:51 +0900

    Create Map.js

---
+++ Map.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    Image,
+    TouchableHighlight,
+  } from 'react-native';
+  import Ionicon from "react-native-vector-icons/Ionicons";
+  import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+function Map({navigation}) {
+  return (<View style={{flex: 1}}>
+      <Text>This is Map Page!</Text>
+      </View>
+  );
+}
+
+export default Map;
