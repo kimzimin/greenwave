01fbbe824a03b1912562f50cb4d4aea13e8d4e2f
che122 <106135369+che122@users.noreply.github.com>
2022-06-15 18:01:27 +0900

    Update Map.js

--- Map.js
+++ Map.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import {StyleSheet,
-    Text,
-    View,
-    TouchableOpacity,
-    Image,
-    TouchableHighlight,
-  } from 'react-native';
-  import Ionicon from "react-native-vector-icons/Ionicons";
-  import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import * as React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
-function Map({navigation}) {
-  return (<View style={{flex: 1}}>
-      <Text>This is Map Page!</Text>
-      </View>
+export default function Map() {
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
   );
 }
 
-export default Map;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
